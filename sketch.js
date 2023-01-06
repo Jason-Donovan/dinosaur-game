3d0531ccccb0872f8f02251b0e2162f4b5c4b5a7
Jason-Donovan <jackpmartin2006@gmail.com>
2023-01-06 15:28:54 +0000

    visual improvement
    - centered all elements on website
    -- button
    -- screen
    -- instruction
    - added instruction on how to play

--- sketch.js
+++ sketch.js
@@ -31,10 +31,9 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(600, 600);
+  let canvas = createCanvas(600, 600);
+  canvas.parent("sketch");
   resetSketch();
-  var button = createButton("reset");
-  button.mousePressed(resetSketch);
 }
 
 function draw() {
